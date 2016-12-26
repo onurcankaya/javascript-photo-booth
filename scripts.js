6fd1634bde71aa417d4d978cfadaf682cf0ad219
Onur Cankaya <oecankaya@gmail.com>
2016-12-26 11:55:30 +0100

    Add paint to canvas function

--- scripts.js
+++ scripts.js
@@ -16,4 +16,17 @@ function getVideo() {
     });
 }
 
+function paintToCanvas() {
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+  console.log(width, height);
+
+  canvas.width = width;
+  canvas.height = height;
+
+  return setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height);
+  }, 16);
+}
+
 getVideo();
